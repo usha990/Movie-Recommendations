c1eca9ea92a3654bfdebb666ae310fe0ac4188fa
usha990 <sinchuusha010@gmail.com>
2026-02-25 22:08:10 +0530

    fixed setting syntax

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -62,11 +62,8 @@ WSGI_APPLICATION = 'auth_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR'db.sqlite3',
-        'USER': 'root',
-        'PASSWORD': 'sinchu@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
